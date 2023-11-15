7b2e36ad1765d6ba1207532a2d1a7d052b1024ba
Edwin <rugoogamuedwin5@gmail.com>
2023-11-15 18:08:19 +0300

    navbar

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import LandingPageNavBar from './components/LandingPageNavBar';
 
 function LandingPage(props) {
     return (
         <div>
+            <LandingPageNavBar/>
             <div>home</div>
         </div>
     );
---
+++ src/components/LandingPageNavBar.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import "./LandingPageNavBar.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import { useState, useEffect } from "react";
+
+function LandingPageNavBar(props) {
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 768);
+
+  const handleResize = () => {
+    setIsLargeScreen(window.innerWidth > 768);
+  };
+  const closeOffcanvas = () => {
+    setShowOffcanvas(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isLargeScreen) {
+      closeOffcanvas();
+    }
+  }, [isLargeScreen]);
+  return (
+    <>
+      {["md"].map((expand) => (
+        <Navbar key={expand} expand={expand} className=" mb-3" id="Navbar">
+          <Container fluid>
+            <Navbar.Brand>
+              {" "}
+              <Link to="#" id="Link">
+                {" "}
+                Navbar Offcanvas
+              </Link>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} id="NavToggle" onClick={() => setShowOffcanvas(!showOffcanvas)}/>
+            <Navbar.Offcanvas 
+              // id={`offcanvasNavbar-expand-${expand}`}
+              id="offcanvas"
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+              show={showOffcanvas} onHide={closeOffcanvas}
+            >
+              <Offcanvas.Header closeButton >
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link id="Link2">
+                    {" "}
+                    <Link to="#action1" id="Link">
+                      {" "}
+                      Home
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link id="Link2">
+                    <Link to="#action2" id="Link">
+                      {" "}
+                      About
+                    </Link>
+                  </Nav.Link>
+
+                  {/* yaa */}
+                  <Nav.Link  id="Link2">
+                    <Link to="#action2" id="Link">
+                      {" "}
+                      Services
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link id="Link2">
+                    <Link to="#action2" id="Link">
+                      {" "}
+                      Portfolio
+                    </Link>
+                  </Nav.Link>
+                  <Nav.Link id="Link2">
+                    <Link to="#action2" id="Link">
+                      {" "}
+                      Contact
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default LandingPageNavBar;
